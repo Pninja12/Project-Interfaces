fa6ce9bd0be1d52eb99d7b8aa7356e637d1d7844
Ivan <ivan.emidio@live.com.pt>
2025-10-07 23:38:16 +0100

    added feedback

--- Interfaces Tangiveis/Assets/Scripts/Main controlling.cs
+++ Interfaces Tangiveis/Assets/Scripts/Main controlling.cs
@@ -1,10 +1,10 @@
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using static GameStarter;
-using System.Collections;
 
 public class Maincontrolling : MonoBehaviour
 {
@@ -15,6 +15,7 @@ public class Maincontrolling : MonoBehaviour
     [SerializeField] private TextMeshProUGUI thatOneText;
     [SerializeField] private List<Sprite> imagesList;
     [SerializeField] private List<Sprite> altImagesList;
+
     private int chosenKey1 = 0;
     private int chosenKey2 = 0;
     private int chosenKey3 = 0;
@@ -24,151 +25,139 @@ public class Maincontrolling : MonoBehaviour
     private int howManyRight;
     private int round;
     private int roundFinish;
+    private bool waitingNextRound = false;
 
-
-    void Awake()
-    {
-
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         roundFinish = GameData.rounds;
-        for (int i = 0; i < imagesList.Count; i++)
+        SetupRound();
+
+       
+        for (int i = 0; i < images.Count; i++)
         {
-            allImages.Add(i);
+            var outlines = images[i].GetComponents<Outline>();
+            if (outlines.Length > 1)
+                Debug.LogWarning($"Image {i} tem {outlines.Length} Outline components. Remove os duplicados no Inspector.");
         }
+    }
+
+    void Update()
+    {
+        if (waitingNextRound) return; 
+
+        if (Input.GetButtonDown("First Choice"))
+        {
+            if (round == roundFinish) { SceneManager.LoadScene("Begin"); return; }
+            chosenKey1++;
+            chosenKey2 = 0;
+            chosenKey3 = 0;
+            Debug.Log("First Choice pressed -> chosenKey1 = " + chosenKey1);
+        }
+        if (Input.GetButtonDown("Second Choice"))
+        {
+            if (round == roundFinish) { SceneManager.LoadScene("Begin"); return; }
+            chosenKey1 = 0;
+            chosenKey2++;
+            chosenKey3 = 0;
+            Debug.Log("Second Choice pressed -> chosenKey2 = " + chosenKey2);
+        }
+        if (Input.GetButtonDown("Third Choice"))
+        {
+            if (round == roundFinish) { SceneManager.LoadScene("Begin"); return; }
+            chosenKey1 = 0;
+            chosenKey2 = 0;
+            chosenKey3++;
+            Debug.Log("Third Choice pressed -> chosenKey3 = " + chosenKey3);
+        }
+
+        
         for (int i = 0; i < 3; i++)
         {
-            int chosen = Random.Range(0, allImages.Count);
-            chosenImagesList.Add(allImages[chosen]);
+            bool selected = (i == 0 && chosenKey1 == 1) ||
+                            (i == 1 && chosenKey2 == 1) ||
+                            (i == 2 && chosenKey3 == 1);
 
+            if (selected)
+            {
+                images[i].sprite = altImagesList[chosenImagesList[i]];
+                SetOutline(images[i], true, Color.yellow);
+            }
+            else
+            {
+                images[i].sprite = imagesList[chosenImagesList[i]];
+                SetOutline(images[i], false, Color.clear);
+            }
+        }
 
-            allImages.Remove(chosenImagesList[i]);
+       
+        if (chosenKey1 == 2 || chosenKey2 == 2 || chosenKey3 == 2)
+        {
+            int chosenIndex = chosenKey1 == 2 ? 0 : chosenKey2 == 2 ? 1 : 2;
+            bool isCorrect = (chosenIndex == correctChoice);
+
+            Debug.Log($"Confirm choice index={chosenIndex} correct={correctChoice} isCorrect={isCorrect}");
+
+            if (isCorrect) howManyRight++;
+
+            StartCoroutine(FlashAndProceed(chosenIndex, isCorrect));
+        }
+    }
+
+    private void SetupRound()
+    {
+        chosenImagesList.Clear();
+        allImages.Clear();
+
+        for (int i = 0; i < imagesList.Count; i++)
+            allImages.Add(i);
+
+        for (int i = 0; i < 3; i++)
+        {
+            int index = Random.Range(0, allImages.Count);
+            chosenImagesList.Add(allImages[index]);
+            allImages.RemoveAt(index);
+
             images[i].sprite = imagesList[chosenImagesList[i]];
             uiText[i].text = imagesList[chosenImagesList[i]].name[..^2];
+            SetOutline(images[i], false, Color.clear);
         }
+
         correctChoice = Random.Range(0, chosenImagesList.Count);
         thatOneText.text = imagesList[chosenImagesList[correctChoice]].name[..^2];
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FlashAndProceed(int chosenIndex, bool wasCorrect)
     {
-        //print($"round - {round} / Finish - {roundFinish}");
+        waitingNextRound = true;
 
-        if (Input.GetButtonDown("First Choice"))
-        {
-            if(round == roundFinish)
-                SceneManager.LoadScene("Begin");
-            chosenKey1++;
-            chosenKey2 = 0;
-            chosenKey3 = 0;
-        }
-        if (Input.GetButtonDown("Second Choice"))
-        {
-            if(round == roundFinish)
-                SceneManager.LoadScene("Begin");
-            chosenKey1 = 0;
-            chosenKey2++;
-            chosenKey3 = 0;
-        }
-        if (Input.GetButtonDown("Third Choice"))
-        {
-            if(round == roundFinish)
-                SceneManager.LoadScene("Begin");
-            chosenKey1 = 0;
-            chosenKey2 = 0;
-            chosenKey3++;
-        }
+        
+        Color original = images[chosenIndex].color;
+        images[chosenIndex].color = wasCorrect ? Color.green : Color.red;
+        yield return new WaitForSeconds(0.9f); 
+        images[chosenIndex].color = original;
 
+        
+        chosenKey1 = chosenKey2 = chosenKey3 = 0;
 
-        if (chosenKey1 == 1)
+        
+        if (round < roundFinish - 1)
         {
-            images[0].sprite = altImagesList[chosenImagesList[0]];
+            SetupRound();
         }
-        else if (chosenKey1 == 0)
+        else
         {
-            images[0].sprite = imagesList[chosenImagesList[0]];
+            foreach (GameObject child in uiTextGameObject)
+                child.SetActive(false);
+
+            foreach (GameObject child in imagesObject)
+                child.SetActive(false);
+
+            thatOneText.text = $"You finished with {howManyRight} rights";
+            StartCoroutine(WaitADamnMinute());
         }
 
-        if (chosenKey2 == 1)
-        {
-            images[1].sprite = altImagesList[chosenImagesList[1]];
-        }
-        else if (chosenKey2 == 0)
-        {
-            images[1].sprite = imagesList[chosenImagesList[1]];
-        }
-
-        if (chosenKey3 == 1)
-        {
-            images[2].sprite = altImagesList[chosenImagesList[2]];
-        }
-        else if (chosenKey3 == 0)
-        {
-            images[2].sprite = imagesList[chosenImagesList[2]];
-        }
-
-        //children[i - 1].GetChild(0).gameObject.SetActive(true);
-        //children[i - 1].gameObject.SetActive(false);
-
-        if (chosenKey1 == 2 || chosenKey2 == 2 || chosenKey3 == 2)
-        {
-            if (correctChoice == 0 && chosenKey1 == 2)
-            {
-                howManyRight++;
-            }
-            if (correctChoice == 1 && chosenKey2 == 2)
-            {
-                howManyRight++;
-            }
-            if (correctChoice == 2 && chosenKey3 == 2)
-            {
-                howManyRight++;
-            }
-
-            chosenKey1 = 0;
-            chosenKey2 = 0;
-            chosenKey3 = 0;
-
-            if (round < roundFinish - 1)
-            {
-
-                chosenImagesList.Clear();
-                allImages.Clear();
-                for (int i = 0; i < imagesList.Count; i++)
-                {
-                    allImages.Add(i);
-                }
-                for (int i = 0; i < 3; i++)
-                {
-
-                    chosenImagesList.Add(allImages[Random.Range(0, allImages.Count)]);
-                    allImages.Remove(chosenImagesList[i]);
-                    images[i].sprite = imagesList[chosenImagesList[i]];
-                    images[i].GetComponentInChildren<TextMeshProUGUI>().text = imagesList[chosenImagesList[i]].name[..^2];
-                }
-                correctChoice = Random.Range(0, chosenImagesList.Count);
-                thatOneText.text = imagesList[chosenImagesList[correctChoice]].name[..^2];
-            }
-            else
-            {
-                foreach (GameObject child in uiTextGameObject)
-                {
-                    child.SetActive(false);
-                    //children[3].GetComponentInChildren<Text>().transform.position.y = 0;
-                }
-                foreach (GameObject child in imagesObject)
-                    child.SetActive(false);
-
-                thatOneText.text = $"You finished with {howManyRight} rights";
-                StartCoroutine(WaitADamnMinute());
-            }
-
-
-            round++;
-        }
+        round++;
+        waitingNextRound = false;
     }
 
     IEnumerator WaitADamnMinute()
@@ -176,6 +165,19 @@ public class Maincontrolling : MonoBehaviour
         yield return new WaitForSeconds(2f);
     }
 
+    private void SetOutline(Image img, bool enabled, Color color)
+    {
+        
+        Outline outline = img.GetComponent<Outline>();
 
-    
+        if (outline == null && enabled)
+            outline = img.gameObject.AddComponent<Outline>();
+
+        if (outline != null)
+        {
+            outline.effectColor = Color.black;
+            outline.effectDistance = new Vector2(8, 8);
+            outline.enabled = enabled;
+        }
+    }
 }
